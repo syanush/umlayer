30d3fc4ea73c6a5a21a01555e03202625af3ccf2
Serguei Yanush <syanush@gmail.com>
2021-10-09 19:36:30 +0300

    Add MainWindow

--- umlayer/gui/mainwindow.py
+++ umlayer/gui/mainwindow.py
@@ -1,1 +1,22 @@
-# stub
+from PySide6.QtWidgets import *
+
+
+# Our main window class
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initGUI()
+
+    def initGUI(self):
+        self.setWindowTitle("UMLayer")
+        self.show()
+        print("Main window initialized.\n")
+
+    def center(self):
+        """ Function to center the main window
+        """
+
+        qRect = self.frameGeometry()
+        centerPoint = self.screen().availableGeometry().center()
+        qRect.moveCenter(centerPoint)
+        self.move(qRect.topLeft())
--- umlayer/main.py
+++ umlayer/main.py
@@ -1,8 +1,26 @@
-from gui import app, mainwindow
+import sys
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+from gui import app
+from gui.mainwindow import MainWindow
 
 
 def main():
-    pass
+    try:
+        # print(PySide6.__version__)
+        # print(PySide6.QtCore.__version__)
+        myApp = QApplication(sys.argv)
+        myWindow = MainWindow()
+        myWindow.center()
+        myApp.exec()
+        sys.exit(0)
+    except SystemExit:
+        print("Closing MainWindow...")
+    except Exception:
+        print(sys.exc_info()[1])
 
 
 if __name__ == '__main__':
